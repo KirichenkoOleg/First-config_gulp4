0659622f0ccaa4d4087633cbc916abc893a28f97
KirichenkoOleg <strannik.kirichenko136@gmail.com>
2020-01-17 00:28:48 +0200

    configurate css, clean, build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 //require - это команда для подключения
-const { src, dest, parallel } = require('gulp'); //загружаем, подключаем gulp
+const { src, dest, series, parallel } = require('gulp'); //загружаем, подключаем gulp
 const uglify = require('gulp-uglify'); //подкл. пакет минифицирования .js файлов
 const concat = require('gulp-concat'); //склеивает файлы, в один
-// const CleanCSS = require('clean-css'); //сжимает css файлы
+const cleanCSS = require('gulp-clean-css'); //сжимает css файлы
+const del = require('del'); //удаление папки перед сборкой
 // const imagemin = require('gulp-imagemin'); //оптимизация изображений
 // const clean = require('gulp-clean'); //удаляет файл или папку
 // const shell = require('gulp-shell'); //очередность запуска
 // const browserSync = require('browser-sync').create();
 // const reload = browserSync.reload; //перезегрузка сервера
 // const runSequence = require('run-sequence'); //запускает задачи по очереди
-// const sourcemaps = require ('gulp-sourcemaps'); //позволяет дебажить минифицированный код в браузере
+const sourcemaps = require ('gulp-sourcemaps'); //позволяет дебажить минифицированный код в браузере
 
 const path = {
 	source: {
@@ -38,6 +39,7 @@ const path = {
 	}
 };
 
+
 function html() {
 	return src(path.source.html)
 		.pipe(dest(path.build.html));
@@ -45,22 +47,23 @@ function html() {
 
 function js() {
 	return src(path.source.js)
-	.pipe(uglify())
-	.pipe(concat('main.js'))
-	.pipe(dest(path.build.js));
-	// .pipe(reload({stream:true}));
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		.pipe(concat('main.js'))
+		.pipe(sourcemaps.write())
+		.pipe(dest(path.build.js));
+		// .pipe(reload({stream:true}));
 }
 
-// gulp.task('css', function() {
-// 	return gulp.src(path.source.styles)
-// 		.pipe(sourcemaps.init())// активация sourcemaps
-// 		.pipe(CleanCSS())
-// 		.pipe(concat('main.css'))
-// 		.pipe(sourcemaps.write())// активация sourcemaps
-// 		.pipe(gulp.dest(path.build.css))
-// 		.pipe(reload({stream:true}));
-// });
-// function 
+function css() {
+	return src(path.source.styles)
+		.pipe(sourcemaps.init())// активация sourcemaps
+		.pipe(cleanCSS())
+		.pipe(concat('main.css'))
+		.pipe(sourcemaps.write())// активация sourcemaps
+		.pipe(dest(path.build.css));
+		// .pipe(reload({stream:true}));
+}
 
 
 // gulp.task('fonts', function() {
@@ -86,10 +89,9 @@ function js() {
 // 		.pipe(gulp.dest(path.build.image));
 // });
 
-// gulp.task('clean', function() {
-// 	return gulp.src('build')
-// 		.pipe(clean());
-// });//задача удаления файла или папки
+function cleanFolder() {
+	return del(['build']);
+};//задача удаления файла или папки
 
 // gulp.task('build', shell.task([
 // 	'gulp clean',
@@ -121,6 +123,8 @@ function js() {
 // gulp.task('default', ('server') ); //программа по умолчанию
 
 exports.js = js;
-// exports.css = css;
+exports.css = css;
 exports.html = html;
-// exports.default = parallel(html, css, js);
+exports.cleanFolder = cleanFolder;
+exports.build  = series(cleanFolder, parallel(html, css, js));
+exports.default  = series(cleanFolder, parallel(html, css, js));
